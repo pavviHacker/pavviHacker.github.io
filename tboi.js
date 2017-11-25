15cdb15a627cc02704990393c746b4436a5edab1
pavviHacker <ricky1640@gmail.com>
2017-11-25 16:19:55 +0100

    Update tboi.js

--- tboi.js
+++ tboi.js
@@ -1,10 +1,11 @@
 function setup(){
-createCanvas(100, 100, WEBGL);
+createCanvas(500, 500, WEBGL);
 }
 
 function draw(){
   background(200);
   rotateX(frameCount * 0.01);
   rotateY(frameCount * 0.01);
+  fill(255);
   box(200, 200, 200);
 }
